bdfa2f3b3ed32d8259c158c90e03cd33b7d16b6a
salwanidabudimanksu <salwanidabudiman@yahoo.com>
2025-04-28 22:30:28 -0400

    Update age_check.py

--- age_check.py
+++ age_check.py
@@ -1,6 +1,11 @@
+# Ask the user to type their age
 age = int(input("Enter your age: "))
 
+# Check if the age is 18 or older
 if age >= 18:
+    # If yes, print that they are an adult
     print("You are an adult.")
 else:
+    # If not, print that they are a minor
     print("You are a minor.")
+#last updated by Salwanida Budiman to Python#
